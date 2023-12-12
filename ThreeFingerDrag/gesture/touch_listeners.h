4ef236c55a8d74a5ffc0a5191ed2775a22c5fba8
Austin Nixholm <pelico1111@hotmail.com>
2023-12-12 15:13:22 -0800

    Improved movement interpolation method
    - dragging now uses IDW (inverse distance weighting) for multivariate interpolation of touch movements
    - improved single finger movement when dragging

--- ThreeFingerDrag/gesture/touch_listeners.h
+++ ThreeFingerDrag/gesture/touch_listeners.h
@@ -2,156 +2,182 @@
 #include "../event/touch_events.h"
 #include "../mouse/cursor.h"
 #include "../notification/popups.h"
+#include <array>
+#include <numeric>
 
-namespace GestureListeners {
+namespace GestureListeners
+{
+    constexpr auto NUM_TOUCH_CONTACTS_REQUIRED = 3;
+    constexpr auto MIN_VALID_TOUCH_CONTACTS = 1;
+    constexpr auto INACTIVITY_THRESHOLD_MS = 50;
+    constexpr auto GESTURE_START_THRESHOLD_MS = 20;
+    constexpr auto CANCELLATION_TIME_MS = 650;
 
-	constexpr auto NUM_TOUCH_CONTACTS_REQUIRED = 3;
-	constexpr auto MIN_VALID_TOUCH_CONTACTS = 1;
-	constexpr auto INACTIVITY_THRESHOLD_MS = 50;
-	constexpr auto GESTURE_START_THRESHOLD_MS = 20;
-	constexpr auto CANCELLATION_TIME_MS = 650;
+    static void CancelGesture()
+    {
+        GlobalConfig* config = GlobalConfig::GetInstance();
+        Cursor::LeftMouseUp();
+        config->SetDragging(false);
+        config->SetCancellationStarted(false);
+    }
 
-	static void CancelGesture() {
-		GlobalConfig* config = GlobalConfig::GetInstance();
-		Cursor::LeftMouseUp();
-		config->SetDragging(false);
-		config->SetCancellationStarted(false);
-	}
+    class TouchActivityListener
+    {
+    public:
+        void OnTouchActivity(TouchActivityEventArgs args)
+        {
+            GlobalConfig* config = GlobalConfig::GetInstance();
 
-	class TouchActivityListener {
-	public:
-		void OnTouchActivity(TouchActivityEventArgs args) {
-			GlobalConfig* config = GlobalConfig::GetInstance();
+            // Check if it's the initial gesture
+            const bool is_initial_gesture = !config->IsDragging() && args.data->can_perform_gesture;
+            const auto time = args.time;
 
-			// Check if it's the initial gesture
-			const bool is_initial_gesture = !config->IsDragging() && args.data->can_perform_gesture;
-			const auto time = args.time;
+            // If it's the initial gesture, set the gesture start time
+            if (is_initial_gesture && !config->IsGestureStarted())
+            {
+                config->SetGestureStarted(true);
+                gesture_start_ = time;
+            }
 
-			// If it's the initial gesture, set the gesture start time
-			if (is_initial_gesture && !config->IsGestureStarted()) {
-				config->SetGestureStarted(true);
-				gesture_start_ = time;
-			}
+            // If there's no previous data, return
+            if (args.previous_data.contacts.empty())
+                return;
 
-			// If there's no previous data, return
-			if (args.previous_data.contacts.empty())
-				return;
+            // Calculate the time elapsed since the initial touchpad contact
+            std::chrono::duration<float> duration = time - gesture_start_;
+            const float ms_since_start = duration.count() * 1000.0f;
 
-			// Calculate the time elapsed since the initial touchpad contact
-			std::chrono::duration<float> duration = time - gesture_start_;
-			const float ms_since_start = duration.count() * 1000.0f;
+            // Calculate the time elapsed since previous touchpad data was received
+            duration = time - config->GetLastGesture();
+            const float ms_since_last_gesture = duration.count() * 1000.0f;
 
-			// Calculate the time elapsed since previous touchpad data was received
-			duration = time - config->GetLastGesture();
-			const float ms_since_last_gesture = duration.count() * 1000.0f;
+            // Prevents initial jumpy movement due to old data comparison
+            if (ms_since_last_gesture > INACTIVITY_THRESHOLD_MS)
+                return;
+            
+            // Delay initial dragging gesture movement, and ignore invalid movement actions
+            if (ms_since_start <= GESTURE_START_THRESHOLD_MS || !(args.data->can_perform_gesture || config->
+                IsDragging()))
+                return;
+            
+            // Loop through each touch contact
+            int valid_touches = 0;
+            std::array<double, NUM_TOUCH_CONTACTS_REQUIRED> accumulated_delta_x = {0.0};
+            std::array<double, NUM_TOUCH_CONTACTS_REQUIRED> accumulated_delta_y = {0.0};
+            std::array<double, NUM_TOUCH_CONTACTS_REQUIRED> distances = {0.0};
+            double total_weight = 0.0;
+            for (int i = 0; i < NUM_TOUCH_CONTACTS_REQUIRED; i++)
+            {
+                if (i > args.previous_data.contacts.size() - 1)
+                    continue;
 
-			// Prevents initial jumpy movement due to old data comparison
-			if (ms_since_last_gesture > INACTIVITY_THRESHOLD_MS) {
-				return;
-			}
+                const auto& contact = args.data->contacts[i];
+                const auto& previous_contact = args.previous_data.contacts[i];
 
-			// Loop through each touch contact
-			int valid_touches = 0;
-			for (int i = 0; i < NUM_TOUCH_CONTACTS_REQUIRED; i++)
-			{
-				if (i > args.previous_data.contacts.size() - 1) 
-					continue;
-				
-				const auto& contact = args.data->contacts[i];
-				const auto& previous_contact = args.previous_data.contacts[i];
+                // Only compare identical touch contact points
+                if (contact.contact_id != previous_contact.contact_id)
+                    continue;
 
-				// Only compare identical touch contact points
-				if (contact.contact_id != previous_contact.contact_id)
-					continue;
+                // Calculate the movement delta for the current finger
+                const double x_diff = contact.x - previous_contact.x;
+                const double y_diff = contact.y - previous_contact.y;
 
-				// Calculate the movement delta for the current finger
-				const double x_diff = contact.x - previous_contact.x;
-				const double y_diff = contact.y - previous_contact.y;
+                // Check if any movement was present since the last received raw input
+                if (std::abs(x_diff) > 0 || std::abs(y_diff) > 0)
+                {
+                    // Cancel immediately if a previous cancellation has begun, and this is a non-gesture movement
+                    if (config->IsCancellationStarted() && !args.data->can_perform_gesture)
+                    {
+                        CancelGesture();
+                        return;
+                    }
+                    
+                    // Accumulate the movement delta for the current finger
+                    accumulated_delta_x[i] += x_diff;
+                    accumulated_delta_y[i] += y_diff;
+                    valid_touches++;
 
-				// Check if any movement was present since the last received raw input
-				if (std::abs(x_diff) > 0 || std::abs(y_diff) > 0)
-				{
-					// Cancel immediately if a previous cancellation has begun, and this is a non-gesture movement
-					if (config->IsCancellationStarted() && !args.data->can_perform_gesture) {
-						CancelGesture();
-						return;
-					}
+                    // Calculate the distance of the current finger from the previous position
+                    distances[i] = std::sqrt(x_diff * x_diff + y_diff * y_diff);
 
-					// Accumulate the movement delta for the current finger
-					accumulated_delta_x_ += x_diff;
-					accumulated_delta_y_ += y_diff;
-					valid_touches++;
-				}
-			}
+                    // If only one finger is moving significantly more than the others
+                    if (distances[i] > 3.0) {
+                        // Assign it a weight of 1 and the other fingers a weight of 0
+                        total_weight = 1.0;
+                        total_weight += total_weight;
+                    } else {
+                        // Calculate the weight of the current finger based on the inverse of the distance
+                        total_weight = 1.0 / distances[i];
+                        total_weight += total_weight;
+                    }
 
-			// If there are not enough valid touches, return
-			if (valid_touches < MIN_VALID_TOUCH_CONTACTS)
-				return;
+                    // Multiply the accumulated deltas by the weight
+                    accumulated_delta_x[i] *= total_weight;
+                    accumulated_delta_y[i] *= total_weight;
+                }
+            }
 
-			// Centroid calculation
-			const long double divisor = static_cast<long double>(NUM_TOUCH_CONTACTS_REQUIRED);
+            // If there are not enough valid touches, return
+            if (valid_touches < MIN_VALID_TOUCH_CONTACTS)
+                return;
+            
+            const double final_delta_x = std::accumulate(accumulated_delta_x.begin(), accumulated_delta_x.end(), 0.0) / total_weight;
+            const double final_delta_y = std::accumulate(accumulated_delta_y.begin(), accumulated_delta_y.end(), 0.0) / total_weight;
 
-			accumulated_delta_x_ /= divisor;
-			accumulated_delta_y_ /= divisor;
+            // Apply movement acceleration 
+            const double gesture_speed = config->GetGestureSpeed() / 100;
+            const double total_delta_x = final_delta_x * gesture_speed;
+            const double total_delta_y = final_delta_y * gesture_speed;
 
-			// Apply movement acceleration 
-			const double gesture_speed = config->GetGestureSpeed() / 100.0;
-			const double total_delta_x = accumulated_delta_x_ * gesture_speed;
-			const double total_delta_y = accumulated_delta_y_ * gesture_speed;
+            config->SetCancellationStarted(false);
+            
+            // Move the mouse pointer based on the calculated vector
+            Cursor::MoveCursor(total_delta_x, total_delta_y);
 
-			// Delay initial dragging gesture movement, and ignore invalid movement actions
-			if (ms_since_start <= GESTURE_START_THRESHOLD_MS || !(args.data->can_perform_gesture || config->IsDragging())) 
-				return;
-			
-			config->SetCancellationStarted(false);
+            const auto change = std::abs(total_delta_x + total_delta_y);
 
-			// Move the mouse pointer based on the calculated vector
-			Cursor::MoveCursor(total_delta_x, total_delta_y);
+            // Set timestamp for when any cursor movement occurred
+            if (change > 0)
+                config->SetLastValidMovement(time);
 
-			const auto change = std::abs(total_delta_x + total_delta_y);
-			
-			// Set timestamp for when any cursor movement occurred
-			if (change > 0)
-				config->SetLastValidMovement(time);
+            // Start dragging if left mouse is not already down.
+            if (!config->IsDragging())
+            {
+                Cursor::LeftMouseDown();
+                config->SetDragging(true);
+            }
+        }
 
-			// Start dragging if left mouse is not already down.
-			if (!config->IsDragging()) {
-				Cursor::LeftMouseDown();
-				config->SetDragging(true);
-			}
-		}
+    private:  
+        // Time point for gesture start
+        std::chrono::time_point<std::chrono::steady_clock> gesture_start_;
+    };
 
-	private:
-		// Accumulated deltas for x and y movements
-		double accumulated_delta_x_ = 0;
-		double accumulated_delta_y_ = 0;
-		// Time point for gesture start
-		std::chrono::time_point<std::chrono::steady_clock> gesture_start_;
-	};
+    class TouchUpListener
+    {
+    public:
+        void OnTouchUp(TouchUpEventArgs args)
+        {
+            GlobalConfig* config = GlobalConfig::GetInstance();
+            config->SetGestureStarted(false);
 
-	class TouchUpListener {
-	public:
-		void OnTouchUp(TouchUpEventArgs args) {
-			GlobalConfig* config = GlobalConfig::GetInstance();
-			config->SetGestureStarted(false);
+            if (!config->IsDragging() || config->IsCancellationStarted())
+                return;
 
-			if (!config->IsDragging() || config->IsCancellationStarted()) 
-				return;
-			
-			// Calculate the time elapsed since the last valid gesture movement
-			const auto now = std::chrono::high_resolution_clock::now();
-			const std::chrono::duration<float> duration = now - config->GetLastValidMovement();
-			const float ms_since_last_movement = duration.count() * 1000.0f;
+            // Calculate the time elapsed since the last valid gesture movement
+            const auto now = std::chrono::high_resolution_clock::now();
+            const std::chrono::duration<float> duration = now - config->GetLastValidMovement();
+            const float ms_since_last_movement = duration.count() * 1000.0f;
 
-			// If there hasn't been any movement for same amount of time we will delay, then cancel immediately
-			if (ms_since_last_movement >= CANCELLATION_TIME_MS) {
-				CancelGesture();
-				return;
-			}
+            // If there hasn't been any movement for same amount of time we will delay, then cancel immediately
+            if (ms_since_last_movement >= CANCELLATION_TIME_MS)
+            {
+                CancelGesture();
+                return;
+            }
 
-			config->SetCancellationStarted(true);
-			config->SetCancellationTime(now);
-		}
-	};
+            config->SetCancellationStarted(true);
+            config->SetCancellationTime(now);
+        }
+    };
 }
-
